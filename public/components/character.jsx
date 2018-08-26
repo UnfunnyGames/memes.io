b0e80290626cbd1809780264a26d3c5b12f3c159
AlbertQM <king4spam@ymail.com>
2018-08-26 13:27:37 +0100

    Click on character will highlight it

--- public/components/character.jsx
+++ public/components/character.jsx
@@ -4,16 +4,18 @@ class Character extends React.Component {
 
   render() {
     return (
-          <div className='avatar' style={this.setAvatar()} ></div>
+          <div className='avatar' onClick={() => this.props.onClick(this.props.character)} style={this.setAvatar()} ></div>
     );
   }
 
   setAvatar() {
      let url = this.props.character.url
+     let border = this.props.character.border
      return ({
        background: `url(${url})`,
        backgroundSize: "cover",
-       backgroundColor: "#B11312"
+       backgroundColor: "#B11312",
+       border: `3px solid ${border}`
      });
    };
 
--- public/components/charactersMenu.jsx
+++ public/components/charactersMenu.jsx
@@ -4,31 +4,46 @@ import Character from "./character.jsx";
 class CharactersMenu extends React.Component {
   state = {
     characters: [
-      { name: "grim", url: "https://avatarfiles.alphacoders.com/120/120045.jpg" },
-      { name: "carlos", url: "https://i.imgur.com/Iq0WeCd.png" },
-      { name: "elf", url: "https://i.imgur.com/RIMs7Yu.png"},
-      { name: "fedora", url: "https://i.imgur.com/mcRwDan.png"},
-      { name: "roll safe", url:"https://i.imgur.com/EUGKFsO.png"}
+      { name: "grim", url: "https://avatarfiles.alphacoders.com/120/120045.jpg", border: "black"},
+      { name: "carlos", url: "https://i.imgur.com/Iq0WeCd.png", border: "black"},
+      { name: "elf", url: "https://i.imgur.com/RIMs7Yu.png", border: "black"},
+      { name: "fedora", url: "https://i.imgur.com/mcRwDan.png", border: "black"},
+      { name: "roll safe", url: "https://i.imgur.com/EUGKFsO.png", border: "black"}
     ]
   };
 
   render() {
-
     return (
       <React.Fragment>
         <div className="wrapper">
           <h1>Select your player:</h1>
-        <div className="character_selection-container">
-          {this.state.characters.map(char => (
-            <Character className="avatar" key={char.id} character={char} />
-          ))}
+          <div className="character_selection-container">
+            {this.state.characters.map(char => (
+              <Character
+                className="avatar"
+                key={char.id}
+                onClick={this.handleClick}
+                character={char}
+              />
+            ))}
           </div>
           <span className="nicknameInput">Nickname: </span>
-          <input type="text" className="nicknameInput"></input>
+          <input type="text" className="nicknameInput" />
         </div>
       </React.Fragment>
     );
   }
+
+  handleClick = char => {
+    console.log("run")
+    let chars = [...this.state.characters];
+    let idx = chars.indexOf(char);
+    chars.forEach((el) => {
+      el.border = "black"
+    })
+    chars[idx].border = "white"
+    this.setState({characters : chars})
+  };
 }
 
 export default CharactersMenu;
